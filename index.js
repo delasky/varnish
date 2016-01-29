b7487a647b5404fe8fb230859d5c58b5a2570e0f
Jon Rogozen <jrogozen@hotpads.com>
2016-01-29 14:10:22 -0800

    add comments

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// enable babel transpiling for the rest of the codebase (reads .babelrc)
 require('babel-core/register');
 
+// require the project
 var server = require('./server');
 
+// start the server
 server.listen();
--- server.js
+++ server.js
@@ -2,15 +2,31 @@ import express from 'express'
 
 const app = express()
 
+// keep an instance of the server in memory so we pass it around or close it
 let server = null
 
-const listen = (callback) => {
+const listen = () => {
   return new Promise((resolve, reject) => {
     server = app.listen(1337, () => {
       console.log('starting server on port 1337')
+
       resolve()
     })
   })
 }
 
-export { app, listen }
+const close = (callback) => {
+    console.log('Shutting down server');
+
+    // possible errors if we open and close express servers in rapid succession
+    // wait a little bit before closing the server
+    setTimeout(() => {
+        if (server) {
+            server.close(callback);
+        } else {
+            callback()
+        }
+    }, 250)
+};
+
+export { app, listen, close }
